84195a103fcd01ef9dd19c31c55ffdf642df8adb
Shogo Watanabe <shogo.watanabe@dena.com>
2017-06-02 18:38:51 +0900

    テストが成功するよう修正

--- src/test/java/jp/sample/HelloWorldTest.java
+++ src/test/java/jp/sample/HelloWorldTest.java
@@ -1,12 +1,14 @@
 package jp.sample;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class HelloWorldTest {
 
 	@Test
 	public void testSeyHello() throws Exception {
-		throw new RuntimeException("not yet implemented");
+		// throw new RuntimeException("not yet implemented");
+		Assert.assertEquals("Hello World!!", new HelloWorld().seyHello());
 	}
 
 }
