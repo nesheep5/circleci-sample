e1429fd78ba8d7581cb3145aa79c30c176880f00
Shogo Watanabe <shogo.watanabe@dena.com>
2017-06-02 18:16:40 +0900

    サンプルメソッド実装

---
+++ src/main/java/jp/sample/HelloWorld.java
@@ -0,0 +1,8 @@
+package jp.sample;
+
+public class HelloWorld {
+
+	public String seyHello() {
+		return "Hello World!!";
+	}
+}
---
+++ src/test/java/jp/sample/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package jp.sample;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void testSeyHello() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
